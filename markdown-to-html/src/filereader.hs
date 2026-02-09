1bcc8b67f7ca874c753731c3b336bf56037f189b
Daaaniell <sungdaniel20@gmail.com>
2026-02-09 11:37:22 +0100

    imported files from before creating this repo

---
+++ markdown-to-html/src/filereader.hs
@@ -0,0 +1,40 @@
+import Data.Maybe
+import System.Environment
+
+-- https://gist.github.com/friedbrice/ef852a5c61e80686d659024aad3cbd70
+-- https://hackage-content.haskell.org/package/base-4.22.0.0/docs/Prelude.html#v:readFile
+-- https://stackoverflow.com/questions/15317895/correct-syntax-for-if-statements-in-haskell
+
+-- checkForFilepath :: String -> Bool
+-- checkForFilepath path =
+-- checkForFilepath path = isValid (path)
+isEmpty :: [a] -> Bool
+isEmpty array = case array of
+  [] -> True
+  [array] -> False
+  _ -> False
+
+maybeArg :: [String] -> Maybe String
+maybeArg array =
+  if null array
+    then Nothing
+    else Just (head array) -- don't know how to get rid of that partial function warning
+
+-- need to check if array is empty or not, otherwise i cant use the just/nothing
+
+-- main = do
+--   [args] <- getArgs
+--   if not (isEmpty args)
+--     then do
+--       file <- readFile args
+--       putStrLn file
+--     else do
+--       putStrLn "invalid path"
+
+main = do
+  args <- getArgs
+  case maybeArg args of
+    Nothing -> putStrLn "invalid args"
+    Just foundPath -> do
+      file <- readFile foundPath
+      putStrLn file
--- markdown-to-html/src/hello-world.hs
+++ markdown-to-html/src/hello-world.hs
@@ -1,2 +1,3 @@
 
 main = putStrLn "hello world!"
+
